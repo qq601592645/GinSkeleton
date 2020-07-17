8a8c054991a70207dc8471ad2aaa3ca38d38dc96
tw <tanwen@do1.com.cn>
2020-07-17 16:58:10 +0800

    jenkins

--- script/jenkins_build.sh
+++ script/jenkins_build.sh
@@ -1,7 +1,7 @@
 export GOPROXY=https://goproxy.io
 export GOROOT=/var/jenkins_home/plugins/go13
 export PATH=$PATH:$GOROOT/bin
-
+export GO111MODULE=on
 export DOCKER_PKG_PATH=./dockerpkg
 export GOOS=linux
 export GOARCH=amd64
