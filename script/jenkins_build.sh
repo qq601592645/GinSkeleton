7df7c3dbeb0cc0a845fae258d96734359b4e7ba7
tw <tanwen@do1.com.cn>
2020-07-17 16:09:18 +0800

    jenkins

--- script/jenkins_build.sh
+++ script/jenkins_build.sh
@@ -2,14 +2,14 @@ export GOPROXY=https://goproxy.io
 export GOROOT=/var/jenkins_home/plugins/go13
 export PATH=$PATH:$GOROOT/bin
 export GOPATH=`pwd`
-export DOCKER_PKG_PATH=.$GOPATH/dockerpkg
+export DOCKER_PKG_PATH=./dockerpkg
 export GOOS=linux
 export GOARCH=amd64
 export GO15VENDOREXPERIMENT=1
 export CGO_ENABLED=0
 
-cd Cmd/Web
 
-go build -ldflags "-X main.Version=1.1.0 -X main.Build=202005" -o $DOCKER_PKG_PATH/bin/tiforg
+
+go build Cmd/Web/mian.go  -ldflags "-X main.Version=1.1.0 -X main.Build=202005" -o $DOCKER_PKG_PATH/bin/tiforg
 
 chmod +x $DOCKER_PKG_PATH/bin/tiforg
